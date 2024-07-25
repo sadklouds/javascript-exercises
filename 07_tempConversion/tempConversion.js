669ea67834a972c3eca9c9d30eb5941147a1fa41
sadkloud <112272752+sadklouds@users.noreply.github.com>
2024-07-25 14:52:45 +0100

    Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let result = (tempFahrenheit - 32) * (5/9);
+  return Math.round(result * 1e1) / 1e1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function( tempCelsius) {
+   let result = ((9/5) * tempCelsius) + 32;
+   return Math.round(result * 1e1) / 1e1;
 };
 
 // Do not edit below this line
