7c2349a80f1f7bee90c1dd409e451d8219aba09e
sadkloud <112272752+sadklouds@users.noreply.github.com>
2024-07-25 12:31:37 +0100

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //const args = Array.from(arguments);
+    result = array.filter(item => !args.includes(item));
+    return result;
 };
 
 // Do not edit below this line
