e1787634c9e5d65ea546c8bbc9357d9dd026527f
sadkloud <112272752+sadklouds@users.noreply.github.com>
2024-07-25 11:26:55 +0100

    Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, iterations) {
+    let result = "";
 
+    if (iterations < 0){
+        return "ERROR";
+    }
+
+    for (let i = 0; i < iterations; i++){
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
