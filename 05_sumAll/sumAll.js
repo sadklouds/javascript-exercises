43706b92f118180e183402de26e9dbe5b668e406
sadkloud <112272752+sadklouds@users.noreply.github.com>
2024-07-25 13:14:36 +0100

    Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    let result = 0;
+
+    if (start < 0 || end < 0){
+        return "ERROR";
+    }
+      
+    if (!Number.isInteger(start)  || !Number.isInteger(end)){
+        return "ERROR";
+    }
+
+    if (start > end){
+        for (let i = end; i <= start; i++){
+            result += i;
+        }
+    }else{
+        for (let i = start; i <= end; i++){
+            result += i;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
